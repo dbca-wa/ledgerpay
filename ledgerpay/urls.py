f8a6301a1cce089b1ee1cfe6b9fbd6b0355b821e
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-01-22 09:41:57 +0800

    Min structure (only for frontend)

--- ledgerpay/urls.py
+++ ledgerpay/urls.py
@@ -1,21 +1,58 @@
-"""ledgerpay URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf import settings
 from django.contrib import admin
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from rest_framework import routers
+from ledgerpay import views, api
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+from ledger.urls import urlpatterns as ledger_patterns
+from ledgerpay.management.default_data_manager import DefaultDataManager
+from ledgerpay.utils import are_migrations_running
+
+# API patterns
+router = routers.DefaultRouter()
+# router.register(r'ledgerpays',api.FeeWaiverViewSet)
+# router.register(r'ledgerpays_paginated',api.FeeWaiverPaginatedViewSet)
+# router.register(r'participants',api.ParticipantsViewSet)
+# router.register(r'parks',api.ParkViewSet)
+# router.register(r'campgrounds',api.CampGroundViewSet)
+# router.register(r'temporary_document', api.TemporaryDocumentCollectionViewSet)
+
+api_patterns = [
+    #url(r'^api/profile$', users_api.GetProfile.as_view(), name='get-profile'),
+    #url(r'^api/department_users$', users_api.DepartmentUserList.as_view(), name='department-users-list'),
+    #url(r'^api/filtered_users$', users_api.UserListFilterView.as_view(), name='filtered_users'),
+    url(r'^api/', include(router.urls)),
 ]
+
+# URL Patterns
+urlpatterns = [
+                  url(r'^ledger/admin/', admin.site.urls, name='ledger_admin'),
+                  url(r'', include(api_patterns)),
+                  url(r'^$', views.LedgerPayRoutingView.as_view(), name='ds_home'),
+                  # url(r'^contact/', views.FeeWaiverContactView.as_view(), name='ds_contact'),
+                  # url(r'^admin_data/', views.FeeWaiverAdminDataView.as_view(), name='admin_data'),
+                  # url(r'^further_info/', views.FeeWaiverFurtherInformationView.as_view(), name='ds_further_info'),
+                  url(r'^internal/', views.InternalView.as_view(), name='internal'),
+                  url(r'^external/', views.ExternalView.as_view(), name='external'),
+                  # url(r'^account/$', views.ExternalView.as_view(), name='manage-account'),
+                  # url(r'^profiles/', views.ExternalView.as_view(), name='manage-profiles'),
+                  # url(r'^help/(?P<application_type>[^/]+)/(?P<help_type>[^/]+)/$', views.HelpView.as_view(), name='help'),
+                  # url(r'^mgt-commands/$', views.ManagementCommandsView.as_view(), name='mgt-commands'),
+                  # url(r'^internal/fee_waiver/(?P<ledgerpay_pk>\d+)/$', views.InternalFeeWaiverView.as_view(), name='internal-ledgerpay-detail'),
+                  # url(r'^history/fee_waiver/(?P<pk>\d+)/$', views.FeeWaiverHistoryCompareView.as_view(), name='ledgerpay_history'),
+              ] + ledger_patterns
+
+if settings.DEBUG:  # Serve media locally in development.
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.SHOW_DEBUG_TOOLBAR:
+    import debug_toolbar
+    urlpatterns = [
+                      url('__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns
+
+
+if not are_migrations_running():
+    DefaultDataManager()
+
