779253244e1046cce350e0857dd2db5c1cd3d2bf
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2021-01-25 11:31:40 +0800

    Add PaymentItem model to store payment items and their URLs

--- ledgerpay/models.py
+++ ledgerpay/models.py
@@ -1,3 +1,14 @@
+from django.db import models
 
 
+class PaymentItem(models.Model):
+    display_name = models.CharField(max_length=100)
+    description = models.TextField()
+    api_url = models.URLField(max_length=200)
 
+    class Meta:
+        app_label = 'ledgerpay'
+
+
+import reversion
+reversion.register(PaymentItem)
